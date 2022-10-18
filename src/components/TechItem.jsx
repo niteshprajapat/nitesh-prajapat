b834c4af900bb8e9f939e5ec1230768d4121142c
Nitesh Prajapat <niteshp282000@gmail.com>
2022-10-18 15:03:21 +0530

    icons updation

--- src/components/TechItem.jsx
+++ src/components/TechItem.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 import techData from "../data/techData";
 
-const TechItem = ({title, icon}) =>{
-    return(
-        <div className=" p-5 hover:-translate-y-2 transform transition duration-100 ease-in-out ">
+const TechItem = ({ title, icon }) => {
+    return (
+        <div className=" p-5 hover:animate-bounce cursor-pointer transform transition duration-200 ease-in-out ">
             <div className="w-8 h-8 mb-3 cursor-pointer">
                 {icon}
             </div>
